3bbd72e708a951548eed087db60fcd725cb9c5a3
Edson Alcala <edson_alcala@msn.com>
2020-02-13 10:25:24 +0000

    added package wasm

---
+++ pkg/eth2-simulator.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} total_eth_staked 
+* @param {number} average_validators_online 
+* @returns {number} 
+*/
+export function get_validator_reward(total_eth_staked: number, average_validators_online: number): number;
---
+++ pkg/eth2-simulator_bg.d.ts
@@ -0,0 +1,4 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function get_validator_reward(a: number, b: number): number;
