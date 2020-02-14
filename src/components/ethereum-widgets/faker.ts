08620c37ed667cc5a8840aa1afcc716b20c45b86
Edson Alcala <edson_alcala@msn.com>
2020-02-14 16:02:12 +0000

    prettier

--- src/components/ethereum-widgets/faker.ts
+++ src/components/ethereum-widgets/faker.ts
@@ -1,76 +1,76 @@
 export const buildFakeSimulationData = () => {
-    return [
-        {
-            time: 1,
-            net_rewards: 10,
-            penalties: 1,
-            rewards: 11
-        },
-        {
-            time: 2,
-            net_rewards: 12,
-            penalties: 2,
-            rewards: 14
-        },
-        {
-            time: 3,
-            net_rewards: 14,
-            penalties: 3,
-            rewards: 17
-        },
-        {
-            time: 4,
-            net_rewards: 16,
-            penalties: 4,
-            rewards: 20
-        },
-        {
-            time: 5,
-            net_rewards: 18,
-            penalties: 6,
-            rewards: 24
-        },
-        {
-            time: 6,
-            net_rewards: 20,
-            penalties: 7,
-            rewards: 27
-        },
-        {
-            time: 7,
-            net_rewards: 22,
-            penalties: 7,
-            rewards: 29
-        },
-        {
-            time: 8,
-            net_rewards: 24,
-            penalties: 8,
-            rewards: 32
-        },
-        {
-            time: 9,
-            net_rewards: 26,
-            penalties: 9,
-            rewards: 35
-        },
-        {
-            time: 10,
-            net_rewards: 38,
-            penalties: 10,
-            rewards: 48
-        },
-        {
-            time: 11,
-            net_rewards: 40,
-            penalties: 11,
-            rewards: 51
-        },
-        {
-            time: 12,
-            net_rewards: 42,
-            penalties: 13,
-            rewards: 55
-        }
-    ]
+  return [
+    {
+      time: 1,
+      net_rewards: 10,
+      penalties: 1,
+      rewards: 11
+    },
+    {
+      time: 2,
+      net_rewards: 12,
+      penalties: 2,
+      rewards: 14
+    },
+    {
+      time: 3,
+      net_rewards: 14,
+      penalties: 3,
+      rewards: 17
+    },
+    {
+      time: 4,
+      net_rewards: 16,
+      penalties: 4,
+      rewards: 20
+    },
+    {
+      time: 5,
+      net_rewards: 18,
+      penalties: 6,
+      rewards: 24
+    },
+    {
+      time: 6,
+      net_rewards: 20,
+      penalties: 7,
+      rewards: 27
+    },
+    {
+      time: 7,
+      net_rewards: 22,
+      penalties: 7,
+      rewards: 29
+    },
+    {
+      time: 8,
+      net_rewards: 24,
+      penalties: 8,
+      rewards: 32
+    },
+    {
+      time: 9,
+      net_rewards: 26,
+      penalties: 9,
+      rewards: 35
+    },
+    {
+      time: 10,
+      net_rewards: 38,
+      penalties: 10,
+      rewards: 48
+    },
+    {
+      time: 11,
+      net_rewards: 40,
+      penalties: 11,
+      rewards: 51
+    },
+    {
+      time: 12,
+      net_rewards: 42,
+      penalties: 13,
+      rewards: 55
+    }
+  ]
 }
